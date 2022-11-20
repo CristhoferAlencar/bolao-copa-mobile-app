405858994e6a1b9a0d7fd81eb0b53e95436517eb
Cristhofer Alencar <cristhofer-alencar@hotmail.com>
2022-11-20 14:07:57 -0300

    Created base layout componets and screens

---
+++ src/screens/Polls.tsx
@@ -0,0 +1,26 @@
+import { Icon, VStack } from "native-base";
+import { Button } from "../components/Button";
+import { Header } from "../components/Header";
+import { Octicons } from '@expo/vector-icons'
+
+export const Polls = () => {
+  return (
+    <VStack flex={1} bg="gray.900">
+      <Header title="Meus bolões" />
+
+      <VStack
+        mt={6}
+        mx={5}
+        borderBottomWidth={1}
+        borderBottomColor="gray.600"
+        pb={4}
+        mb={4}
+      >
+        <Button
+            title="BUSCAR BOLÃO POR CÓDIGO"
+            leftIcon={<Icon as={Octicons} name="search" color="black" size="md" />}
+        />
+      </VStack>
+    </VStack>
+  );
+};
